adcd02f3f7936e0f0a1267a6b98350ec4f04ad7c
lostexee <leonsampl10@gmail.com>
2025-11-10 08:05:19 +0100

    A1

--- A1_ZweiThreadsZaehlenWinner/Program.cs
+++ A1_ZweiThreadsZaehlenWinner/Program.cs
@@ -5,22 +5,40 @@ namespace A1_ZweiThreadsZaehlenWinner;
 
 class Program
 {
-   
-    
+
+
     public static void Main(string[] args)
     {
         Console.WriteLine("Übung 1: Zwei Threads – Zählen & Winner");
-        
-        
+        Thread thA = new Thread(() => CountUpThreadA());
+        Thread thB = new Thread(() => CountDownThreadB());
+
+        thA.Start();
+        thB.Start();
+
+        thA.Join();
+        thB.Join();
     }
-    
+
     private static void CountUpThreadA()
     {
-        
+        int miliseconds = 100;
+
+        for (int i = 1; i >= 100; i++)
+        {
+            Console.WriteLine(i);
+            Thread.Sleep(miliseconds);
+        }
     }
-    
+
     private static void CountDownThreadB()
     {
-       
+        int miliseconds = 100;
+
+        for (int i = 100; i <= 1; i--)
+        {
+            Console.WriteLine(i);
+            Thread.Sleep(miliseconds);
+        }
     }
 }
